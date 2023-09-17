811b5aed7305db80305bfdd92014b9bd7ea4e980
Michael <michaellngriffin@gmail.com>
2023-09-17 05:08:20 -0400

    user model complete, testing needed

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@
 
 const bcrypt = require("bcrypt");
 const { DB_URI, SECRET_KEY, BCRYPT_WORK_FACTOR } = require("../config");
+const db = require("../db");
 
 class User {
 
@@ -11,21 +12,23 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
+  //FIXME: register needs to set last_login as well.
   static async register({ username, password, first_name, last_name, phone }) {
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
-
+    const join_at = new Date(); //This solved it: easy enough.
     const results = await db.query(
       `INSERT INTO users (username,
                           password,
                           first_name,
                           last_name,
-                          phone)
-      VALUES
-        ($1, $2, $3, $4, $5)`,
-        [username, hashedPassword, first_name, last_name, phone]
+                          phone,
+                          join_at)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING username, password, first_name, last_name, phone`,
+        [username, hashedPassword, first_name, last_name, phone, join_at]
     );
 
-    const user = new User(results.rows[0]);
+    const user = results.rows[0];
 
     return user;
   }
@@ -44,14 +47,31 @@ class User {
   }
 
   /** Update last_login_at for user */
-
   static async updateLoginTimestamp(username) {
+    let mostRecentLogin = new Date();//payload.iat;
+
+    const results = await db.query(
+      `UPDATE users
+      SET last_login_at=$1
+      WHERE username = $2`,
+      [mostRecentLogin, username]
+    );
+    return {username, last_login: mostRecentLogin};
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const results = await db.query(
+      `SELECT username,
+          first_name,
+          last_name,
+        FROM users
+        ORDER BY last_name, first_name`
+    );
+
+    return results.rows;
   }
 
   /** Get: get user by username
@@ -64,6 +84,18 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const results = await db.query(
+      `SELECT username,
+        first_name,
+        last_name,
+        phone,
+        join_at,
+        last_login_at
+      FROM users
+      WHERE username = $1`,
+      [username]
+    );
+    return results.rows[0];
   }
 
   /** Return messages from this user.
@@ -75,6 +107,22 @@ class User {
    */
 
   static async messagesFrom(username) {
+    const results = await db.query(`
+    SELECT id, body, sent_at, read_at,
+      users.username AS username,
+      users.first_name AS first_name,
+      users.last_name AS last_name,
+      users.phone AS phone,
+    FROM messages as m
+      JOIN users ON messages.to_username = users.username
+    WHERE from_username = $1`,
+    [username]);
+
+    return results.rows.map(result => {
+      let formatted = {id, body, sent_at, read_at};
+      formatted["to_user"] = {username, first_name, last_name, phone};
+      return formatted;
+    });
   }
 
   /** Return messages to this user.
@@ -86,6 +134,22 @@ class User {
    */
 
   static async messagesTo(username) {
+    const results = await db.query(`
+    SELECT id, body, sent_at, read_at,
+      users.username AS username,
+      users.first_name AS first_name,
+      users.last_name AS last_name,
+      users.phone AS phone,
+    FROM messages as m
+      JOIN users ON messages.from_username = users.username
+    WHERE from_username = $1`,
+    [username]);
+
+    return results.rows.map(result => {
+      let formatted = {id, body, sent_at, read_at};
+      formatted["from_user"] = {username, first_name, last_name, phone};
+      return formatted;
+    });
   }
 }
 
